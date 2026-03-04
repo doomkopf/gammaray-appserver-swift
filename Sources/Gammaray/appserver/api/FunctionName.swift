b934be766cb49f7813c287232966241e5db1b246
doomkopf <rhodium-tally0i@icloud.com>
2026-03-04 10:24:51 +0100

    Allowing function name length of 1

--- Sources/Gammaray/appserver/api/FunctionName.swift
+++ Sources/Gammaray/appserver/api/FunctionName.swift
@@ -2,7 +2,7 @@ struct FunctionName: Hashable, CustomStringConvertible {
     let value: String
 
     init(_ value: String) throws {
-        if try validate(str: value, minLength: 3, maxLength: 64) {
+        if try validate(str: value, minLength: 1, maxLength: 64) {
             self.value = value
         } else {
             throw AppError.General("Invalid function name: \(value)")
